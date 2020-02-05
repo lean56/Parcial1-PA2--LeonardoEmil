7bc9702f6ade947fd9a5877cbdd8bcf5b252809b
leonardo emil <theflaco56@hotmail.com>
2020-02-04 20:47:43 -0400

    Creando Contexto

---
+++ Parcial1-PA2/DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_PA2.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=EMIL56\SQLEXPRESS; Database=Parcial1DB; trusted_connection=true");
+        }
+    }
+}
